8147bac4678eb689aea8453118b2a0a181c1a896
TitouanLeBret <154740047+TitouanLeBret@users.noreply.github.com>
2024-12-10 17:04:27 +0100

    ajout static files

--- siteCourse/settings.py
+++ siteCourse/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
-
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -225,10 +225,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "siteCourse/static")
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#dossier de des pour collectstatic
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#dossier de dest pour collectstatic
 
 
 # Default primary key field type
